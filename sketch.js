dae05f73923e39d4c7616e03f04b5d7b8ebadfec
Aekantvpatil <73891522+Aekantvpatil@users.noreply.github.com>
2021-01-23 10:17:45 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,59 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+var paper1,ground1,dumper1,dumper2,dumper3
+
+
+function setup() {
+	createCanvas(1500, 700);
+
+	
+
+	engine = Engine.create();
+	world = engine.world;
+	paper1=new paper(200,450,40)
+	ground1=new ground(600,500,1800,20)
+
+	//Create the Bodies Here.
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+	World.add(world, ground);
+	boxPosition=width/2-100 
+	boxY=610;
+	boxleftSprite=createSprite(1050, 440, 20,100);
+	 boxleftSprite.shapeColor=color(255,0,0);
+	 boxLeftBody = Bodies.rectangle(1050,440, 20,100 , {isStatic:true} );
+	  World.add(world, boxLeftBody);
+	  boxBase=createSprite(1150, 480, 180,20);
+	   boxBase.shapeColor=color(255,0,0);
+	 boxBottomBody = Bodies.rectangle(1150, 480, 180,20 , {isStatic:true} );
+	World.add(world, boxBottomBody);
+	boxleftSprite=createSprite(1250,440,20,100);
+	 boxleftSprite.shapeColor=color(255,0,0);
+boxRightBody = Bodies.rectangle(1250,440,20,100,{isStatic:true});
+World.add(world, boxRightBody);
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  
+	paper1.display();
+	ground1.display();
+
+  drawSprites();
+ 
+}
+
+function keyPressed() {
+	if(keyCode===UP_ARROW){
+		Matter.Body.applyForce(paper1.body,paper1.body.position,{x:85,y:-85});
+	}
+}
+
+
